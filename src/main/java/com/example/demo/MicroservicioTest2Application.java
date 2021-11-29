513fff70113990cd33f72e0a04f3109a5f67d106
Esau <esauhp97@gmail.com>
2021-11-29 13:46:38 +0100

    First commit

---
+++ src/main/java/com/example/demo/MicroservicioTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioTest2Application.class, args);
+	}
+
+}
